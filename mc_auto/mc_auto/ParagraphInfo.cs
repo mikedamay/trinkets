4ea18b70f0b20037bfc0586c91cb111c284fcfca
Mike May <mikemay.mdamay@googlemail.com>
2012-12-18 08:40:53 +0000

    working with groups 3

--- mc_auto/mc_auto/ParagraphInfo.cs
+++ mc_auto/mc_auto/ParagraphInfo.cs
@@ -8,161 +8,51 @@ using ExtensionMethods;
 
 namespace mc_auto
 {
-    internal class ParagraphInfo
+    internal partial class ParagraphInfo
     {
         private const string MC_TECH_STYLE = "mc_tech";
-        private class Strategy
-        {
-            internal static readonly Strategy Default = new Strategy(
-                    st: (xw, pi) =>
-                    {
-                        xw.WriteStartElement("p");
-                        xw.WriteAttributeString("level", pi.Level.ToString());
-                        xw.WriteString(MassageXMLString(pi.Paragraph.Range.Text));
-                    }
-
-                    , en: (xw,pi) => xw.WriteEndElement()
-                    , isc: (child,parent) => (int)parent.Paragraph.OutlineLevel < (int)child.Level
-                    , gl: (pi) => (int)pi.Paragraph.OutlineLevel
-            );
-            public Strategy(Starter st, Ender en, IsChilder isc, GetLeveler gl)
-            {
-                DoStart = st;
-                DoEnd = en;
-                DoIsChild = isc;
-                DoGetLevel = gl;
-            }
-            internal readonly Starter DoStart;
-            internal readonly Ender DoEnd;
-            internal readonly IsChilder DoIsChild;
-            internal readonly GetLeveler DoGetLevel;
-        }
-        private class StrategyKey
-        {
-            private static readonly IDictionary<string, bool> paragraphInfos = new Dictionary<string, bool>()
-            {
-                {MC_TECH_STYLE, true}
-                ,{"mc_example_text", true}
-            };
-
-
-            private int hash;
-
-            public StrategyKey(WrapperType wt) : this(wt, 0, string.Empty, string.Empty)
-            {                
-            }
-            public StrategyKey(int outlineLevel)
-                : this(WrapperType.None, outlineLevel, string.Empty, string.Empty)
-            {
-            }
-            public StrategyKey(string style, string previousStyle)
-                : this(WrapperType.None, 0, style, previousStyle)
-            {
-
-            }
-            public StrategyKey(WrapperType wt, int outlineLevel, string style, string previousStyle)
-            {
-                hash = Hash(wt) > 0 ? Hash(wt) 
-                  : Hash(outlineLevel) > 0 ? Hash(outlineLevel) 
-                  : Hash(style, previousStyle);
-            }
-
-            private int Hash(string style, string previousStyle)
-            {
-                int st = (paragraphInfos.GetValueOrDefault(style, false) ? 0x1 : 0x0) << 3;
-                int st2 = (paragraphInfos.GetValueOrDefault(previousStyle, false) ? 0x1 : 0x0) << 4;
-                return st | st2;
-            }
-
-            private int Hash(int outlineLevel)
-            {
-                return (outlineLevel < BODY_LEVEL && outlineLevel > 0 ? 0x1 : 0x0) << 2;
-            }
-
-            private int Hash(WrapperType wt)
-            {
-                return wt == WrapperType.EndMarker ? 0x01 : wt == WrapperType.Root ? (0x1<<1) : 0x00;
-            }
-            // override object.Equals
-            public override bool Equals (object obj)
-            {
-                if (obj == null || GetType() != obj.GetType()) 
-                {
-                    return false;
-                }
-        
-                return hash == (obj as StrategyKey).hash; 
-            }
-    
-            // override object.GetHashCode
-            public override int GetHashCode()
-            {
-                return hash;
-            }
-        }
         private delegate void Starter(System.Xml.XmlWriter xw, ParagraphInfo pi);
         private delegate void Ender(System.Xml.XmlWriter xw, ParagraphInfo pi);
         private delegate bool IsChilder(ParagraphInfo pi, ParagraphInfo piParent);
         private delegate int GetLeveler(ParagraphInfo pi);
 
-        private static Starter NoopStart = (xw, pi) => { };
-        private static Ender NoopEnd = (xw, pi) => { };
-        private static IsChilder TrueIsChild = (p, pp) => true;
-        private static IsChilder FalseIsChild = (p, pp) => false;
-        private static GetLeveler SuperParent = pi => int.MinValue;
-        private static Ender StdEnd = (xw, pi) => xw.WriteEndElement();
-        private static IsChilder StdIsChild = (child, parent) => (int)parent.Paragraph.OutlineLevel < (int)child.Level;
-        private static GetLeveler StdGetLevel = (pi) => (int) pi.Paragraph.OutlineLevel;
+        private static readonly Starter NoopStart = (xw, pi) => { };
+        private static readonly Ender NoopEnd = (xw, pi) => { };
+        private static readonly IsChilder TrueIsChild = (p, pp) => true;
+        private static readonly IsChilder FalseIsChild = (p, pp) => false;
+        private static readonly GetLeveler SuperParent = pi => int.MinValue;
+        private static readonly Ender StdEnd = (xw, pi) => xw.WriteEndElement();
+        private static readonly IsChilder StdIsChild = (child, parent) => (int)parent.Paragraph.OutlineLevel < (int)child.Level;
+        private static readonly GetLeveler StdGetLevel = (pi) => (int)pi.Paragraph.OutlineLevel;
+        private static readonly Starter StdStart = (xw, pi) =>
+        {
+            xw.WriteStartElement("p");
+            xw.WriteAttributeString("level", pi.Level.ToString());
+            xw.WriteAttributeString("class", pi.Paragraph.Range.ParagraphStyle.NameLocal.ToString());
+            xw.WriteAttributeString("tables", pi.Paragraph.Range.Tables.Count.ToString());
+            xw.WriteAttributeString("row", pi.Paragraph.Range..ToString());
+
+            xw.WriteString(MassageXMLString(pi.Paragraph.Range.Text));
+        };
+        private static readonly Starter ParentStart = (xw, pi) =>
+        {
+            xw.WriteStartElement("p");
+            StdStart(xw, pi);
+            xw.WriteEndElement();
+        };
 
         private static IDictionary<StrategyKey, Strategy> _strategyMap 
           = new Dictionary<StrategyKey, Strategy>()
             {
-                {new StrategyKey(WrapperType.Root)
-                  , new Strategy(st: NoopStart, en: NoopEnd, isc: TrueIsChild, gl: SuperParent )
-                }
-                ,{new StrategyKey(WrapperType.EndMarker)
-                  , new Strategy(st: NoopStart, en: NoopEnd, isc: FalseIsChild, gl: SuperParent )
-                }
-                ,{new StrategyKey(HEADING_1_LEVEL)
-                  , new Strategy(
-                      st: (xw, pi) =>
-                        {
-                            xw.WriteStartElement("p");
-                            xw.WriteStartElement("p");
-                            xw.WriteAttributeString("level", pi.Level.ToString());
-                            xw.WriteString(MassageXMLString(pi.Paragraph.Range.Text));
-                            xw.WriteEndElement();
-                        }
-                      , en: StdEnd
-                      , isc: StdIsChild
-                      , gl: StdGetLevel )
-                }
-                ,{new StrategyKey(BODY_LEVEL)
-                  , new Strategy(
-                      st: (xw, pi) =>
-                        {
-                            xw.WriteStartElement("p");
-                            xw.WriteAttributeString("level", pi.Level.ToString());
-                            xw.WriteString(MassageXMLString(pi.Paragraph.Range.Text));
-                        }
-
-                      , en: StdEnd
-                      , isc: StdIsChild
-                      , gl: StdGetLevel )
-                }
+                {new StrategyKey(WrapperType.Root), new Strategy(st: NoopStart, en: NoopEnd, isc: TrueIsChild, gl: SuperParent )}
+                ,{new StrategyKey(WrapperType.EndMarker), new Strategy(st: NoopStart, en: NoopEnd, isc: FalseIsChild, gl: SuperParent )}
+                ,{new StrategyKey(HEADING_1_LEVEL), new Strategy(st: ParentStart, en: StdEnd, isc: StdIsChild, gl: StdGetLevel )}
+                ,{new StrategyKey(BODY_LEVEL), new Strategy(st: StdStart, en: StdEnd, isc: StdIsChild, gl: StdGetLevel )}
                 ,{new StrategyKey(MC_TECH_STYLE, string.Empty)
-                  , new Strategy(
-                      st: (xw, pi) =>
-                        {
-                            xw.WriteStartElement("p");
-                            xw.WriteStartElement("p");
-                            xw.WriteAttributeString("level", pi.Level.ToString());
-                            xw.WriteString(MassageXMLString(pi.Paragraph.Range.Text));
-                            xw.WriteEndElement();
-                        }
-                      , en: StdEnd
-                      , isc: (child, parent) => (int)parent.Paragraph.Range.ParagraphStyle == child.Paragraph.Range.ParagraphStyle
-                      , gl: StdGetLevel )
+                  , new Strategy(st: ParentStart, en: StdEnd
+                      , isc: (child, parent) => parent.Paragraph.Range.ParagraphStyle.NameLocal == child.Paragraph.Range.ParagraphStyle.NameLocal
+                      , gl: StdGetLevel 
+                      )
                 }
             };
         public enum WrapperType
@@ -230,7 +120,7 @@ namespace mc_auto
         {
             
         }
-        private ParagraphInfo(Word.Paragraph para, Word.Paragraph previousPara, WrapperType wt) :this()
+        private ParagraphInfo(Word.Paragraph para, Word.Paragraph previousPara, WrapperType wt) : this()
         {
             _strategy = _strategyMap.GetValueOrDefault(
               new StrategyKey(wt
