11843c10287f747204c51050a675b74b31ff8d2d
Mike May <mikemay.mdamay@googlemail.com>
2013-02-03 14:10:44 +0000

    failed with intellij now using github for windws

---
+++ quickstart/src/main/java/jar/App.java
@@ -0,0 +1,14 @@
+package jar;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        // testing Git
+        System.out.println( "Hello World!" );
+    }
+}
