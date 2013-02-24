0d7914fcca393e4f8724f4c748b8570e47bcfab9
Mike May <mikemay.mdamay@googlemail.com>
2013-02-24 19:13:03 +0000

    broken github

--- quickstart/src/main/java/jar/App.java
+++ quickstart/src/main/java/jar/App.java
@@ -1,5 +1,8 @@
 package jar;
 
+import java.io.InputStream;
+import java.util.Properties;
+
 /**
  * Hello world!
  *
@@ -8,6 +11,18 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        new App();
+    }
+    private App() {
+        try {
+            Properties prop = new Properties();
+            InputStream in = getClass().getResourceAsStream("/app.properties");
+            prop.load(in);
+            in.close();
+            System.out.println( "Hello " + prop.getProperty("app.name") + "!" );
+        }
+        catch ( Exception ex ) {
+            ex.printStackTrace();
+        }
     }
 }
