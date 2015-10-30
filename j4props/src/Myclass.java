27f78c8d12990582c4052181e86990e72d7c42d8
mike <mikemay@blueyonder.co.uk>
2015-10-30 09:20:05 +0000

    parsing example.simple and elm.simple well

---
+++ j4props/src/Myclass.java
@@ -0,0 +1,13 @@
+/**
+ * Created by mike on 9/20/15.
+ */
+public class Myclass {
+    void fun() {
+        int nn = 5;
+        String bob = "case";
+        switch (nn) {
+            case 5:
+                nn++;
+        }
+    }
+}
