5b95129ebffcba08e7f8557c58bcbfb713da4bb0
mike <mikemay@blueyonder.co.uk>
2017-07-05 09:45:25 +0100

    added new project ParseSpotify

---
+++ ParseSpotify/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ParseSpotify
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
