0f49e03f5c997668ccded76d28ca75fc35006aae
mikedamay <mikemay@blueyonder.co.uk>
2017-07-22 18:43:42 +0100

    Added Drive project

---
+++ Drive/Drive/Platform.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drive
+{
+    internal interface Platform
+    {    
+       string GetHomeDirectoryName();
+    }
+    internal class WindowsPlatform : Platform
+    {
+        public string GetHomeDirectoryName()
+        {
+            return "";
+        }
+    }
+}
