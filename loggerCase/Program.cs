e753f34ac703a7a67c808204837fa9f449577a1b
mike <mikemay@blueyonder.co.uk>
2018-08-02 08:53:33 +0100

    logger case goose chase

---
+++ loggerCase/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+
+public class Program
+{
+    public static void Main()
+    {
+		IHost host = new HostBuilder()
+		.ConfigureLogging(
+			logBuilder => {
+				logBuilder.AddDebug();
+				logBuilder.AddConsole();
+				logBuilder.SetMinimumLevel(LogLevel.Debug);
+			}
+		)
+		.ConfigureServices(
+			(hostContext, services) => services.AddSingleton<Stooge>()
+			
+		)
+		.Build();
+        System.Console.WriteLine("Just stop the logger");
+		var stooge = host.Services.GetService(typeof(Stooge));
+		Console.WriteLine(stooge);
+    }
+}
+
+public class Stooge
+{
+	public Stooge(ILogger<Stooge> logger)
+	{
+		Console.WriteLine("console works in Stooge");
+		logger.LogInformation("nothing to see here");
+		Thread.Sleep(5000);
+	}
+}
