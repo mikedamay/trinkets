627dac7adee797366259ee7cfeed02a4ae79c8cf
mikedamay <mikemay@blueyonder.co.uk>
2018-12-07 07:50:35 +0000

    sorting out workflow for custom sql migrations

--- Nute/Migrations/CustomSql/SundryViews.cs
+++ Nute/Migrations/CustomSql/SundryViews.cs
@@ -11,20 +11,19 @@
     d) Model based migrations should not have dependencies on custom migrations.
 
     From scratch (from clean install of code-base):
-    1. drop all objects from database
+    1. drop all objects from database (but leave the "nutrition" database itself
     2. remove any migrations generated from model (i.e. any migrations that are direct
        children of the Migrations directory) that happen to be hanging about.  Do not
        remove any files in the Migrations/CustomSql directory.
-    3. remove NutritionDbContextModelSnapshot (because this is  a book mark
-       for the current state of of migrations generated from model - we need that to be null)
+    3. remove NutritionDbContextModelSnapshot (because this is a book mark
+       for the current state of of migrations generated from model - we need that to be empty)
     4. execute "dotnet ef migrations add --context NutritionDbContext start" 
        - to generate migrations from model
     5. execute "dotnet ef database update --context NutritionDbContext"
      
     Incremental (how to handle model changes on dev's machine as code evolves):
-    1. Add a static class containing the Up() and Down() in CustomSql. 
-    2. execute "dotnet ef migrations add --context NutritionDbContext <something>" - to generate migrations from model
-    3. execute "dotnet ef database update --context NutritionDbContext"
+    1. execute "dotnet ef migrations add --context NutritionDbContext <something>" - to generate migrations from model
+    2. execute "dotnet ef database update --context NutritionDbContext"
     i.e you can simply operate on the nutrition db context using defaults  Commands
     like "migrations remove" and "database update 0" should work well.
     
